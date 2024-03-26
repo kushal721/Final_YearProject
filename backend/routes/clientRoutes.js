53f56bac37073a6a81601e13318476c8e533eeb4
Kushal721 <bhattakushal1@gmail.com>
2024-03-26 22:38:17 +0545

    third commit

---
+++ backend/routes/clientRoutes.js
@@ -0,0 +1,63 @@
+import express, { request } from "express";
+const router = express.Router();
+import UserController from "../controllers/userController.js";
+import checkUserAuth from "../middlewares/auth-middleware.js";
+
+import UserModel from "../models/User.js";
+
+// Protected Routes
+// router.get("/protected-route", checkUserAuth, (req, res) => {
+//   // Access user data attached to the req object
+//   const data = req.user;
+// });
+
+router.post("/protected-route", checkUserAuth, async (req, res) => {
+  try {
+    // Extract user ID from the request body
+    const { userId } = req.body;
+
+    // Find the user by ID
+    const user = await UserModel.findOne(userId);
+
+    // Check if the user exists
+    if (!user) {
+      return res.status(404).send({ message: "User does not exist", success: false });
+    }
+
+    // If user exists, send user data in the response
+    res.status(200).send({
+      success: true,
+      data: {
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    // Handle internal server error
+    console.error("Error getting user:", error);
+    res.status(500).send({ message: "Error getting user", success: false });
+  }
+});
+
+
+router.use("/loggedUser", checkUserAuth);
+router.use("/user-changePassword", checkUserAuth);
+
+// Public Routes
+router.post("/user-register", UserController.userRegistration);
+router.post("/user-login", UserController.userLogin);
+router.post(
+  "/user-send-reset-password-email",
+  UserController.sendUserPasswordResetEmail
+);
+router.post(
+  "/user-reset-password/:id/:token",
+  UserController.userPasswordReset
+);
+
+// Protected Routes
+router.post("/user-changePassword", UserController.changeUserPassword);
+router.get("/loggedUser", UserController.loggedUser);
+
+export default router;
---
+++ backend/routes/designRoutes.js
@@ -0,0 +1,47 @@
+import express from "express";
+import {
+  addDesigns,
+  getDesigns,
+  getDesign,
+  getMyDesigns,
+  getMyDesign,
+  deleteMyDesign,
+  updateMyDesign,
+  productReview,
+} from "../controllers/designController.js"; // Import named export
+import requireAuth from "../middlewares/requireAuth.js";
+
+const router = express.Router();
+
+//public routes
+
+//GET all designs
+router.get("/", getDesigns);
+
+//GET single designs
+router.get("/:id", getDesign);
+// POST a new design
+router.post("/", addDesigns);
+
+//review product
+
+router.patch("/:id/review", productReview);
+
+//private routes
+
+//require auth for design routes
+router.use(requireAuth);
+
+//GET all designs
+router.get("/", getMyDesigns);
+
+//GET single designs
+router.get("/:id", getMyDesign);
+
+//DELETE a  workout
+router.delete("/:id", deleteMyDesign);
+
+//UPDATE a  workout
+router.patch("/:id", updateMyDesign);
+
+export default router; // Export the router as default
